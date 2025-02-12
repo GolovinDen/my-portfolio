10a7f2706cacf9846d660732138ef67f6d4cd0e7
GolovinDen <denisgolovin99@outlook.com>
2025-02-12 11:01:25 +0300

    first commit

---
+++ src/layout/header/Header.tsx
@@ -0,0 +1,25 @@
+
+import styled from "styled-components";
+import {Logo} from "../../components/logo/Logo.tsx";
+import {Menu} from "../../components/menu/Menu.tsx";
+
+const items =["Home", "Skills", "Works", "Testimony", "Contact"]
+
+
+export const Header = () => {
+    return (
+        <StyledHeader>
+            <Logo/>
+            <Menu menuItems={items}/>
+
+
+        </StyledHeader>
+    );
+};
+
+const StyledHeader = styled.header`
+    background-color: #68fa60;
+    display: flex;
+    justify-content: space-between;
+`
+
---
+++ src/layout/sections/main/Main.tsx
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+import photo from '../../../assets/images/photo.jpg'
+import {FlexWrapper} from "../../../components/FlexWrapper.tsx";
+
+
+export const Main = () => {
+    return (
+        <StyledMain>
+            <FlexWrapper align={"center"} justify={"space-around"} >
+                <div>
+                    <span>Hi There</span>
+                    <Name>I am Denis Golovin</Name>
+                    <MainTitle>A Web Developer</MainTitle>
+                </div>
+
+                <Photo src={photo} alt=""/>
+            </FlexWrapper>
+
+        </StyledMain>
+    );
+};
+
+const StyledMain = styled.div`
+    min-height: 100vh;
+    background-color: #e3d569;
+`
+
+const MainTitle = styled.h1`
+
+`
+
+const Name = styled.h2`
+
+`
+
+
+const Photo = styled.img`
+    width: 350px;
+    height: 430px;
+    object-fit: cover;
+`
+
