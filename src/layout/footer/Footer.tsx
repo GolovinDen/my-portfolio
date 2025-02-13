99f89089a84934b3ac1ee308a596e8fd3b5c344d
GolovinDen <denisgolovin99@outlook.com>
2025-02-13 15:57:41 +0300

    Style 2 complete

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import {Icon} from "../../components/icon/Icon.tsx";
 import {FlexWrapper} from "../../components/FlexWrapper.tsx";
+import {theme} from "../../styles/Theme.ts";
 
 
 export const Footer = () => {
@@ -27,21 +28,24 @@ export const Footer = () => {
                 </SocialList>
                 <Copyright>© 2025 Denis Golovin, All Rights Reserved.</Copyright>
             </FlexWrapper>
-
         </StyledFooter>
     );
 };
 
 const StyledFooter = styled.footer`
-    background-color: #fbb57d;
-    min-height: 20vh;
+    background-color: ${theme.colors.primaryBg};
+    padding: 40px 0;
 `
 const Name = styled.span`
-
+    font-family: "Josefin Sans", sans-serif;
+    font-weight: 700;
+    font-size: 22px;
+    letter-spacing: 3px;
 `
 const SocialList = styled.ul`
     display: flex;
-    gap: 30px;
+    gap: 20px;
+    margin: 30px 0;
     
 `
 const SocialItem = styled.li`
@@ -49,8 +53,27 @@ const SocialItem = styled.li`
 `
 
 const SocialLink = styled.a`
+    background-color: rgba(255, 255, 255, 0.1);
+    border-radius: 50%;
+    width: 35px;
+    height: 35px;
 
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
+    color: ${theme.colors.accent};
+    
+    &:hover {
+        color: ${theme.colors.primaryBg};
+        transform: translateY(-4px);
+        
+}
+    
 `
 const Copyright = styled.small`
-
+    font-weight: 400;
+    font-size: 12px;
+    text-align: center;
+    opacity: 0.5;
 `
