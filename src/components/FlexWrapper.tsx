40b9299aa030e0d53ee708eb830c7e8652adde4f
GolovinDen <denisgolovin99@outlook.com>
2025-02-13 11:13:19 +0300

    MainStyled complete

--- src/components/FlexWrapper.tsx
+++ src/components/FlexWrapper.tsx
@@ -15,4 +15,5 @@ export const FlexWrapper = styled.div<FlexWrapperPropsType>`
     justify-content: ${props => props.justify || "flex-start"};
     align-items: ${props => props.align || "center" };
     flex-wrap: ${props => props.wrap || "nowrap"};
+    height: 100%;
 `
