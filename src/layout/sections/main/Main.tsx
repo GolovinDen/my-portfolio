f58622e83a0fe2ba9a31c83f28cbdb86b3e76c05
GolovinDen <denisgolovin99@outlook.com>
2025-02-19 15:36:03 +0300

    main-media complete

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -3,13 +3,14 @@ import photo from '../../../assets/images/photo.jpg'
 import {FlexWrapper} from "../../../components/FlexWrapper.tsx";
 import {Container} from "../../../components/Container.ts";
 import {theme} from "../../../styles/Theme.ts";
+import {font} from "../../../styles/Common.ts";
 
 
 export const Main = () => {
     return (
         <StyledMain>
             <Container>
-                <FlexWrapper align={"center"} justify={"space-between"}>
+                <FlexWrapper align={"center"} justify={"space-around"} wrap={"wrap"}>
                     <div>
                         <SmallText>Hi There</SmallText>
                         <Name>I am <span>Denis Golovin</span></Name>
@@ -35,6 +36,8 @@ const StyledMain = styled.section`
  const PhotoWrapper = styled.div`
      position: relative;
      z-index: 0;
+     margin-top: 65px;
+     
      &::before {
          content: '';
          width: 360px;
@@ -45,6 +48,13 @@ const StyledMain = styled.section`
          top: -24px;
          left: 24px;
          z-index: -1;
+         
+         @media ${theme.media.mobile} {
+             width: 314px;
+             height: 414px;
+             top: -17px;
+             left: 20px;
+         }
      }
  `
 
@@ -53,23 +63,27 @@ const Photo = styled.img`
     width: 350px;
     height: 430px;
     object-fit: cover;
+    margin-right: 20px;
+    
+    @media ${theme.media.mobile} {
+        width: 310px;
+        height: 380px;
+    }
 `
 
 const MainTitle = styled.h1`
-    font-weight: 400;
-    font-size: 27px;
+    ${font({ weight: 400, Fmax: 27, Fmin: 20})}
 `
 
 const Name = styled.h2`
-    font-family: "Josefin Sans", sans-serif;
-    font-weight: 700;
-    font-size: 50px;
+    ${font({family:" 'Josefin Sans', sans-serif", weight: 700, Fmax: 50, Fmin: 36})}
     letter-spacing: 0.05em;
     margin: 10px 0; 
     
     span {
         position: relative;
         z-index: 0;
+        white-space: nowrap;
         
         &::before {
             content: "";
@@ -81,6 +95,10 @@ const Name = styled.h2`
             bottom: 0;
             z-index: -1;
         }
+    }
+    
+    @media ${theme.media.mobile} {
+        margin: 15px 0 22px;
     }
 `
 const SmallText = styled.span`
--- src/styles/Theme.ts
+++ src/styles/Theme.ts
@@ -15,7 +15,7 @@ export  const theme = {
 
     media: {
         tablet: "screen and (max-width: 768px)",
-        mobile: "screen and (max-width: 576)",
+        mobile: "screen and (max-width: 576px)",
     }
 
 }
